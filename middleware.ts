c04ea7005783fa69f2febf7c8dd77502a34cf8a0
Mudana Komang <admin@balisnaptrip.com>
2024-11-20 14:15:18 +0800

    update auth dan middleware

--- middleware.ts
+++ middleware.ts
@@ -5,30 +5,13 @@ export async function middleware(req: NextRequest) {
   // Ambil token JWT dari permintaan
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  // Jika token tidak ada atau user bukan admin, redirect ke login
+  // Jika token tidak ada atau role bukan 'admin', redirect ke login
   if (!token || token.role !== 'admin') {
     return NextResponse.redirect(new URL('/auth/admin/login', req.url));
   }
 
-  // Jika token ada dan role admin, lanjutkan ke halaman berikutnya
-  if (token) {
-    // Ubah token menjadi string (JSON string) sebelum menyimpan ke cookie
-    const stringifiedToken = JSON.stringify(token);
-    const response = NextResponse.next();
-
-    // Set cookie dengan token yang sudah diserialisasi
-    response.cookies.set('token', stringifiedToken, {
-      httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      path: '/',
-      maxAge: 60 * 60 * 24,
-    });
-
-    return response;
-  }
-
-  return NextResponse.next();
+  // Jika token ada dan role 'admin', lanjutkan ke halaman berikutnya
+  return NextResponse.next(); // Melanjutkan ke halaman yang diminta
 }
 
 export const config = {
